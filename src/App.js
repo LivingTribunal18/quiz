f7dbf893590164cc94f06eec737c63d9b26824ab
karineB <karinebondarenko03@gmail.com>
2021-09-02 12:19:57 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./hoc/layout/layout";
 import Quiz from "./containers/quiz/quiz";
 import QuizCreator from "./containers/quizCreator/quizCreator";
+import DeleteQuiz from "./containers/deleteQuiz/deleteQuiz";
 import QuizList from "./containers/quizList/quizList";
 import Auth from "./containers/auth/auth";
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/auth" component={Auth}></Route>
           <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz-delete" component={DeleteQuiz} />
           <Route path="/quiz/:questionId" component={Quiz} />
           <Route exact path="/" component={QuizList} />
         </Switch>
